2ccd53e51d27613cb14a975598bd206d2b086fae
Tim Hudson <tim.hudson15@gmail.com>
2013-02-22 11:43:39 -0500

    Rewrite Folsom to be a searchable cache

--- index.js
+++ index.js
@@ -1,23 +1,75 @@
-// folsom.js
-// version : 0.0.1
-// author : Tim Hudson
-// license : MIT
+var indexes = {}
 
-(function() {
+module.exports = function(key) {
+  if (indexes[key]) return indexes[key]
+  return indexes[key] = new Folsom()
+}
 
-	var cache = {};
+function Folsom() {
+  this.cache = []
+}
 
-	function folsom(key, value) {
-		if (value === undefined) return cache[key];
-		if (toString.call(key) != '[object String]') return new Error('folsom first parameter must be a string');
+Folsom.prototype.add = function(items) {
+  var self = this;
 
-		cache[key] = value;
-	}
+  if (!Array.isArray(items)) items = [items]
 
-	if (typeof module !== 'undefined' && module.exports) {
-		module.exports = folsom;
-	} else {
-		this.folsom = folsom;
-	}
+  items.forEach(function(item) {
+    self.cache.push(item)
+  })
 
-}).call(this);
+  return this
+}
+
+Folsom.prototype.index = function(key) {
+  this._index = key
+  return this
+}
+
+Folsom.prototype.search = function(query, callback) {
+  var self = this
+    , items = []
+    , beginswith = []
+    , inmiddle = []
+
+  if (!this._index) throw new Error('An index must be defined')
+  if (!callback) throw new Error('Search requires a callback')
+
+  // Filter items by query
+  items = this.cache.filter(function(item) {
+    return ~item[self._index].toLowerCase().indexOf(query.toLowerCase())
+  })
+
+  // Separate items that begin with query from those that do not
+  items.forEach(function(item) {
+    if (!item[self._index].toLowerCase().indexOf(query.toLowerCase())) {
+      beginswith.push(item)
+    } else {
+      inmiddle.push(item)
+    }
+  })
+
+  // Sort each array alphabetically
+  beginswith = sortObjects(beginswith, this._index)
+  inmiddle = sortObjects(inmiddle, this._index)
+
+  // Combine the arrays together again
+  items = beginswith.concat(inmiddle);
+
+  callback(null, items)
+
+  return this
+}
+
+Folsom.prototype.reset = function(items) {
+  this.cache = []
+  this.add(items)
+
+  return this
+}
+
+function sortObjects(array, key) {
+  return array.sort(function(a, b) {
+    return a[key] < b[key] ? -1 : 1;
+  })
+}
